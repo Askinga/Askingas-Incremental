b35cff7dbe6405302748e60428418fde24a7e0ef
Askinga <152022166+Askinga@users.noreply.github.com>
2025-02-22 16:20:11 -0500

    Update upgrades.js

--- js/upgrades.js
+++ js/upgrades.js
@@ -1,7 +1,8 @@
 const up1bought = 0
 
 function buyUpgrade1(){
-  if(up1bought.lt(1)){
+  if(up1bought.lt(1) && clickElement.innerHTML.gte(75)){
     up1bought = up1bought.add(1)
+    clickElement = clickElement.sub(75)
   }
 }
