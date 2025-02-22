d756bc405d9ba53a31f33184df7a1ccd084873cd
Askinga <152022166+Askinga@users.noreply.github.com>
2025-02-21 23:40:42 -0500

    Update game.js

--- game.js
+++ game.js
@@ -5,3 +5,8 @@ let player = {
 function Click(){
   player.clicks = player.clicks.add(1);
 }
+
+function displayText() {
+  var text = 'You have ' format(player.clicks) + ' Clicks'
+  text.style.display = "block";
+}
