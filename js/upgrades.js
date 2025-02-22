4320051f3be3ac557f7bee843346db4438035ee0
Askinga <152022166+Askinga@users.noreply.github.com>
2025-02-22 16:37:58 -0500

    Update upgrades.js

--- js/upgrades.js
+++ js/upgrades.js
@@ -1,11 +1,3 @@
-
-
-function buyUpgrade1() {
-  if (up1bought < 1 && parseInt(clickElement.innerHTML) >= 75) {
-    up1bought += 1;
-    clickElement.innerHTML = parseInt(clickElement.innerHTML) - 75;
-  }
-}
 let up1bought = 0;
 
 function saveGameState2() {
