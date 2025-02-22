f36835c3486d47fcbae2505bd1c4ca7071f8dbb3
Askinga <152022166+Askinga@users.noreply.github.com>
2025-02-22 10:57:47 -0500

    Update game.js

--- game.js
+++ game.js
@@ -3,9 +3,3 @@ let clicks = document.querySelector('.clicks')
 function Click(){
   clicks.innerHTML = parseFloat(clicks.innerHTML) + 1;
 }
-
-function displayText() {
-  var text = 'You have ' format(player.clicks) + ' Clicks';
-  text.style.display = "block";
-}
-
