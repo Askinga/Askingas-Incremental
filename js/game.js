418b52b0b85a52b82bed18d743fc142337a65229
Askinga <152022166+Askinga@users.noreply.github.com>
2025-02-22 23:58:37 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -92,7 +92,6 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function buyUpgrade2() {
-    const currentClicks = parseInt(clickElement.innerHTML, 10);
     if (up2bought < 1 && currentClicks >= 300) {
       up2bought += 1;
       clickElement.innerHTML = currentClicks - 300;
