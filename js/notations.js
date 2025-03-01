a4c0a958352f4f527154a5e8810332f42f445451
Askinga <musicgemca@gmail.com>
2025-03-01 14:50:40 -0500

    Create notations.js

---
+++ js/notations.js
@@ -0,0 +1,12 @@
+function exponentialFormat(num, precision, mantissa = true) {
+    let e = num.log10().floor()
+    let m = num.div(Decimal.pow(10, e))
+    if (m.toStringWithDecimalPlaces(precision) == 10) {
+        m = decimalOne
+        e = e.add(1)
+    }
+    e = (e.gte(1e9) ? format(e, 5) : (e.gte(10000) ? commaFormat(e, 0) : e.toStringWithDecimalPlaces(0)))
+    if (mantissa)
+        return m.toStringWithDecimalPlaces(precision) + "e" + e
+    else return "e" + e
+}
