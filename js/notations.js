bcd0242e10ff3471abbf3168bb3900df51bce903
Askinga <musicgemca@gmail.com>
2025-03-01 21:19:21 -0500

    Update notations.js

--- js/notations.js
+++ js/notations.js
@@ -61,6 +61,13 @@ function format(decimal, precision = 2, small) {
 
 }
 
+function formatWhole(decimal) {
+    decimal = new Decimal(decimal)
+    if (decimal.gte(1e9)) return format(decimal, 2)
+    if (decimal.lte(0.99) && !decimal.eq(0)) return format(decimal, 2)
+    return format(decimal, 0)
+}
+
 function invertOOM(x){
     let e = x.log10().ceil()
     let m = x.div(Decimal.pow(10, e))
